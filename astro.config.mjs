be5b3cfa3f4ae28cd6a3aab53773eb3b07f7d17f
EC2854 <ec2854@proton.me>
2025-05-21 19:42:45 +0200

    ae

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 //
 export default defineConfig({
-  site: 'https://EC2854.github.io',
+  site: 'https://ec2854.github.io',
   base: 'EC2854',
 })
